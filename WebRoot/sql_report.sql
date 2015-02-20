5f773896794794dc43eceb015535b6f89422ff85
yingyanhua <hippocrene814@gmail.com>
2015-02-19 16:08:22 -0800

    part1 sql almost done

--- WebRoot/sql_report.sql
+++ WebRoot/sql_report.sql
@@ -1,22 +1,27 @@
 1.
+-- student info
 SELECT st.first_name as first, st.middle_name as middle, st.last_name as last, st.ssn as ssn
 FROM student st, student_enrollment se
 WHERE st.stu_id = se.stu_id AND se.year = 2009 AND se.quarter = 'SPRING'
 
+-- current class
 SELECT ss.unit as unit, ss.section_id as section_id, c.class_id as class_id, c.title as title, c.course_id as course_id, c.year as year, c.quarter as quarter
 FROM student st, student_section ss, section se, class c
 WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.section_id = se.section_id AND se.class_id = c.class_id AND c.year = 2009 AND c.quarter = 'SPRING'
 
 2.
-SELECT cl.title, cl.year, cl.quarter, co.course_number
+-- class info
+SELECT cl.title as title, cl.year as year, cl.quarter as quarter, co.course_number as course_number
 FROM class cl, course co
 WHERE cl.course_id = co.course_id
 
+-- roster
 SELECT st.stu_id, st.ssn, st.citizen, st.pre_school, st.pre_degree, st.pre_major, st.first_name as first, st.middle_name as middle, st.last_name as last, ss.unit, ss.letter_su
 FROM class cl, section se, student_section ss, student st
 WHERE cl.title = ? AND se.class_id = cl.class_id AND ss.section_id = se.section_id AND ss.stu_id = st.stu_id
 
 3.
+-- student info
 SELECT st.first_name as first, st.middle_name as middle, st.last_name as last, st.ssn as ssn
 FROM student st
 WHERE EXISTS (
@@ -25,12 +30,25 @@ WHERE EXISTS (
     WHERE st.stu_id = se.stu_id
 )
 
+-- for class
+SELECT cl.year, cl.quarter, cl.class_id, cl.title, cl.course_id, ss.grade, ss.unit, ss.letter_su
+FROM student st, student_section ss, section se, class cl
+WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.section_id = se.section_id AND se.class_id = cl.class_id
+ORDER BY cl.year, cl.quarter
+
+-- for GPA
 SELECT cl.year, cl.quarter, SUM(ss.unit * co.grade_num) / SUM(ss.unit)
 FROM student st, student_section ss, section se, class cl, conversion co
-WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND ss.letter_su = 'letter' AND ss.grade = co.grade_letter
+WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND ss.letter_su = 'letter' AND ss.grade <> 'IN' AND ss.grade = co.grade_letter
 GROUP BY cl.year, cl.quarter
 
+-- for total GPA
+SELECT SUM(ss.unit * co.grade_num) / SUM(ss.unit)
+FROM student st, student_section ss, section se, class cl, conversion co
+WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND ss.letter_su = 'letter' AND ss.grade <> 'IN' AND ss.grade = co.grade_letter
+
 4.
+-- undergrad info
 SELECT s.ssn, s.first_name, s.middle_name, s.last_name
 FROM student s, student_enrollment se
 WHERE s.stu_id = se.stu_id AND se.year = 2009 AND se.quarter = 'SPRING'AND NOT EXISTS (
@@ -39,16 +57,26 @@ FROM grad g
 WHERE s.stu_id = g.stu_id
 )
 
+-- undergrad degree
 SELECT name, type
 FROM degree
 WHERE type = 'BE' OR type = 'BA'
 
-SELECT ca.cate_id, dc.min_unit - SUM(ss.unit)
-FROM student_section ss, section se, class cl, course co, degree d, degree_category dc, category ca
-WHERE ss.stu_id = ? AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND cl.course_id = co.course_id AND d.degree_id = ? AND d.degree_id = dc.degree_id AND dc.cate_id = ca.cate_id
-GROUP BY ca.cate_id, dc.min_unit
+-- total remainunit
+SELECT de.total_min_unit - SUM(ss.unit) AS remain_unit
+FROM student st, student_section ss, degree de
+WHERE de.name = ? AND st.ssn = ? AND st.stu_id = ss.stu_id AND ss.grade <> 'IN' AND ss.grade <> 'na'
+GROUP BY de.total_min_unit
+
+-- remain category unit
+SELECT dc.cate_id, dc.min_unit - SUM(ss.unit) AS remain_cate_unit
+FROM student st, student_section ss, section se, class cl, course co, degree de, degree_category dc
+WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.grade <> 'IN' AND ss.grade <> 'na' AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND cl.course_id = co.course_id
+        AND de.name = ? AND de.degree_id = dc.degree_id AND dc.cate_id = co.cate_id
+GROUP BY dc.cate_id, dc.min_unit
 
 5.
+-- grad info
 SELECT s.ssn, s.first_name, s.middle_name, s.last_name
 FROM student s, student_enrollment se
 WHERE s.stu_id = se.stu_id AND se.year = 2009 AND se.quarter = 'SPRING'AND NOT EXISTS (
@@ -57,19 +85,24 @@ FROM undergrad u
 WHERE s.stu_id = u.stu_id
 )
 
+-- grad degree
 SELECT name, type
 FROM degree
 WHERE type = 'MS'
 
-SELECT con.min_unit - SUM(ss.unit) AS diff
-FROM student st, student_section ss, section se, class cl, course_concentration cc, concentration con
-WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND cl.course_id = cc.course_id AND cc.con_id = con.con_id
-GROUP BY con.con_id, con.min_unit
+-- concenctration completed
+SELECT dc.con_id, dc.min_unit, SUM(ss.unit) AS unit, SUM(ss.unit * co.grade_num) / SUM(ss.unit) AS grade
+FROM student st, student_section ss, section se, class cl, course_concentration cc, degree de, degree_concentration dc, conversion co
+WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.grade <> 'IN' AND ss.grade <> 'na' AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND cl.course_id = cc.course_id
+    AND ss.grade = co.grade_letter AND cc.con_id = dc.con_id AND de.name = ? AND de.degree_id = dc.degree_id
+GROUP BY dc.con_id, dc.min_unit, dc.min_grade
+HAVING dc.min_unit - SUM(ss.unit) <= 0 AND dc.min_grade - SUM(ss.unit * co.grade_num) / SUM(ss.unit) <= 0
 
-SELECT dc.min_unit - SUM(ss.unit) AS diff
-FROM student st, student_section ss, section se, class cl, course_concentration cc, degree_concentration dc
-WHERE st.ssn = ? AND st.stu_id = ss.stu_id AND ss.section_id = se.section_id AND se.class_id = cl.class_id AND cl.course_id = cc.course_id AND cc.con_id = dc.con_id
-GROUP BY dc.con_id, dc.min_unit
+-- next available time
+SELECT
+FROM
+WHERE
+GROUP BY
 
 6.
 SELECT st.first_name as first, st.middle_name as middle, st.last_name as last, st.ssn as ssn
